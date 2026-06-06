ae4732b833cd9a9c02e5db2dcb2374a324b32e3b
satyabrata11x <satyabrata11x@gmail.com>
2026-06-06 20:27:44 +0530

    Complete

--- src/main/java/com/Satya/SmartParkingSystem/slot/ParkingSlotRepository.java
+++ src/main/java/com/Satya/SmartParkingSystem/slot/ParkingSlotRepository.java
@@ -2,8 +2,12 @@ package com.Satya.SmartParkingSystem.slot;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ParkingSlotRepository
         extends JpaRepository<ParkingSlot, Long> {
+
     boolean existsByVehicleId(Long vehicleId);
 
+    List<ParkingSlot> findAllByOrderByIdAsc();
 }
